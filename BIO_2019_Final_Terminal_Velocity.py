b9f2138b06f8c162288e51437ba0b47e1f2b25ab
Prince <62904535+princek0@users.noreply.github.com>
2022-04-22 11:35:11 +0100

    Update BIO_2019_Final_Terminal_Velocity.py

--- BIO_2019_Final_Terminal_Velocity.py
+++ BIO_2019_Final_Terminal_Velocity.py
@@ -2,37 +2,35 @@ from itertools import combinations
 from collections import Counter
 
 p = int(input(""))
-data = []
-data_reverse = []
-while (-1, -1) not in data:
+collisions = []
+collisions_reversed = []
+while (-1, -1) not in collisions:
     a, b = input().split(" ")
-    data.append((max(int(a), int(b)), min(int(a), int(b))))
-    data_reverse.append((min(int(a), int(b)), max(int(a), int(b))))
+    collisions.append((max(int(a), int(b)), min(int(a), int(b))))
+    collisions_reversed.append((min(int(a), int(b)), max(int(a), int(b))))
 
-data.pop()
+collisions.pop()
 
-all = list(combinations([i for i in range(1, p+1)],2))
-for i in range(len(data)):
-    if data_reverse[i] in all:
-        all.remove((data_reverse[i]))
+combinations = list(combinations([i for i in range(1, p + 1)], 2))
+for i in range(len(collisions)):
+    if collisions_reversed[i] in all:
+        combinations.remove((collisions_reversed[i]))
 
+for i in range(len(combinations)):
+    if combinations[i] not in collisions:
+        collisions.append(combinations[i])
 
-for i in range(len(all)):
-    if all[i] not in data:
-        data.append(all[i])
+collisions_birds = []
 
-data2 = []
+for i in range(len(collisions)):
+    collisions_birds.append(collisions[i][0])
 
-for i in range(len(data)):
-    data2.append(data[i][0])
-
-counter = dict(Counter(data2))
-for i in range(1, p+1):
+counter = dict(Counter(collisions_birds))
+for i in range(1, p + 1):
     if i not in counter.keys():
         counter[i] = 0
 
-dict2 = {k: v for k, v in sorted(counter.items(), key=lambda item: item[1])}
-print(dict2)
-for i in range(len(list(dict2.keys())[::-1])):
-    print(list(dict2.keys())[::-1][i])
-    
+counter_reversed = {k: v for k, v in sorted(counter.items(), key=lambda item: item[1])}
+
+for i in range(len(list(counter_reversed.keys())[::-1])):
+    print(list(counter_reversed.keys())[::-1][i], sep=' ', end=' ', flush=True)
